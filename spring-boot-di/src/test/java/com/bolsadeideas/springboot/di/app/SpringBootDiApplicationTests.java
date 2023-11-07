5104f51106966ceaa702b662aadaba5714108e9c
lexlion24 <alexanderlionbusiness@gmail.com>
2023-11-07 09:21:04 +0100

    spirng-di arreglado y comienzo de spring-form

---
+++ spring-boot-di/src/test/java/com/bolsadeideas/springboot/di/app/SpringBootDiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bolsadeideas.springboot.di.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class SpringBootDiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
